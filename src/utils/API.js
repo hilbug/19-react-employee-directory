19c14cc153e0507e386981e2fa3f111d12310abf
hilbug <65197724+hilbug@users.noreply.github.com>
2020-10-25 22:37:07 -0400

    add axios and api for random employees

---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+const EMPURL = "https://randomuser.me/api/?results=50&nat=us";
+
+export default {
+  getRandomEmployees: () => axios.get(EMPURL)
+};
+
+// ESLint... https://github.com/benmosher/eslint-plugin-import/blob/v2.22.1/docs/rules/no-anonymous-default-export.md
