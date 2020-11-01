dd8f445d321d3f178889fc8045b517cabb9c1bdb
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-01 17:24:22 -0500

    SearchBox clean-up

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -5,16 +5,13 @@ function SearchBox(props) {
     return (
         <div>
             <h2 className="text-center">Search for employees!</h2>
-            {/* Remove form to get rid of "enter" being usable */}
-            {/* <form className="search form-inline d-flex align-items-center">
-                <div className="form-group mx-auto"> */}
                 <div className="input-group pb-4">
                     <div className="input-group-prepend">
                         <span className="input-group-text">Employee Last Name</span>
                     </div>
                     <input
                         value={props.search}
-                        onChange={props.handleInputChange}  // maybe only need this and not handleformsubmit
+                        onChange={props.handleInputChange}  // only need this and not handleformsubmit
                         name="search"
                         type="search"
                         className="form-control mx-2"
@@ -22,12 +19,11 @@ function SearchBox(props) {
                         id="empsearch"
                     />
                 </div>
-                {/* </div>
-            </form> */}
         </div>
     );
 }
 
 export default SearchBox;
 
+// Notes
 // Week 19 Activity 23-Pupster - SearchForm
